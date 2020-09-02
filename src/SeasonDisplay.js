9c9d9767a238c8ffb341b7a4853b52e65c88f83c
Sunil Kumar <sunil.kumar@metacube.com>
2020-09-03 01:26:37 +0530

    state

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -6,5 +6,4 @@ const SeasonDisplay = () => {
     );
 };
 
-
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from "react";
 import ReactDom from "react-dom";
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+	constructor(props) {
+		super(props);
 
-    return <div>Latitude: </div>;
-  }
+		this.state = { lat: null };
+
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({ lat: position.coords.latitude })
+			},
+			(err) => console.log(err)
+		);
+	}
+
+	render() {
+		return <div>Latitude: {this.state.lat}</div>;
+	}
 }
 
 ReactDom.render(<App />, document.querySelector("#root"));
